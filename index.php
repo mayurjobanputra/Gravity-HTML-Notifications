3197ab6d68819ae29ba9f35a8f86b9204678df3b
Mayur <6332663+mayurjobanputra@users.noreply.github.com>
2023-12-19 19:29:00 -0800

    fatal error

--- index.php
+++ index.php
@@ -6,21 +6,26 @@
  * Author: Mayur
  */
 
-// Check if Gravity Forms is active upon plugin activation
 register_activation_hook( __FILE__, 'mayur_plugin_activation' );
 function mayur_plugin_activation() {
     if ( !class_exists( 'GFForms' ) ) {
-        wp_die('Sorry, but this plugin requires the Gravity Forms to be installed and active.');
+        deactivate_plugins( plugin_basename( __FILE__ ) ); 
+        wp_die('Sorry, but this plugin requires the Gravity Forms to be installed and active.', 'Plugin dependency check', array( 'back_link' => true ));
     }
 }
 
-// Add the admin menu only if Gravity Forms is active
-if ( class_exists( 'GFForms' ) ) {
-    add_action( 'admin_menu', 'mayur_html_notification_menu' );
-}
-
+add_action( 'admin_menu', 'mayur_html_notification_menu' );
 function mayur_html_notification_menu() {
-    GFForms::add_settings_page( 'HTML Notifications', 'mayur_html_notification_settings_page' );
+    if (class_exists('GFForms')) {
+        add_submenu_page(
+            'gf_edit_forms',
+            'HTML Notifications',
+            'HTML Notifications',
+            'manage_options',
+            'mayur_html_notifications',
+            'mayur_html_notification_settings_page'
+        );
+    }
 }
 
 function mayur_html_notification_settings_page() {
@@ -80,20 +85,4 @@ function mayur_customize_gform_email_notification( $notification, $form, $entry 
     $notification['message'] = $html_before . $notification['message'] . $html_after;
 
     return $notification;
-}
-
-add_action( 'admin_menu', 'mayur_html_notification_menu' );
-
-function mayur_html_notification_menu() {
-    if (class_exists('GFForms')) {
-        // Add a submenu under the "Forms" menu in Gravity Forms
-        add_submenu_page(
-            'gf_edit_forms', // The slug of the parent menu (Forms menu)
-            'HTML Notifications', // The page title
-            'HTML Notifications', // The menu title
-            'manage_options', // Capability required
-            'mayur_html_notifications', // Menu slug
-            'mayur_html_notification_settings_page' // Callback function
-        );
-    }
 }
