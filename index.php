55d6dbab904b23de899d494177c0b4c19d597585
Mayur <6332663+mayurjobanputra@users.noreply.github.com>
2023-12-19 19:17:59 -0800

    sigh

--- index.php
+++ index.php
@@ -1,49 +1,50 @@
-
 <?php
 /**
- * Plugin Name: Mayurs HTML Notification Customizer
+ * Plugin Name: Mayurs Gravity Forms HTML Notification Customizer
  * Description: Customize HTML before and after Gravity Forms email notifications.
  * Version: 1.0
  * Author: Mayur
  */
 
-// Check if Gravity Forms is active
-if (!is_plugin_active('gravityforms/gravityforms.php')) {
-    return;
+// Check if Gravity Forms is active upon plugin activation
+register_activation_hook( __FILE__, 'mayur_plugin_activation' );
+function mayur_plugin_activation() {
+    if ( !class_exists( 'GFForms' ) ) {
+        wp_die('Sorry, but this plugin requires the Gravity Forms to be installed and active.');
+    }
 }
 
-add_action( 'admin_menu', 'mayur_html_notification_menu' );
+// Add the admin menu only if Gravity Forms is active
+if ( class_exists( 'GFForms' ) ) {
+    add_action( 'admin_menu', 'mayur_html_notification_menu' );
+}
 
 function mayur_html_notification_menu() {
-    if (class_exists('GFForms')) {
-        GFForms::add_settings_page( 'HTML Notifications', 'mayur_html_notification_settings_page' );
-    }
+    GFForms::add_settings_page( 'HTML Notifications', 'mayur_html_notification_settings_page' );
 }
 
 function mayur_html_notification_settings_page() {
-    // Check if the user has submitted the settings
-    // WordPress will add the "settings-updated" $_GET parameter to the url
-    if ( isset($_GET['settings-updated']) ) {
-        // Add settings saved message with the class of "updated"
-        add_settings_error( 'mayur_messages', 'mayur_message', __( 'Settings Saved', 'mayur' ), 'updated' );
+    if ( !current_user_can( 'manage_options' ) ) {
+        return;
     }
 
-    // Show error/update messages
+    if ( isset($_GET['settings-updated']) ) {
+        add_settings_error( 'mayur_messages', 'mayur_message', 'Settings Saved', 'updated' );
+    }
+
     settings_errors( 'mayur_messages' );
 
-    // Retrieve the stored values
-    $html_before = get_option('mayur_html_before', '');
-    $html_after = get_option('mayur_html_after', '');
+    $html_before = get_option( 'mayur_html_before', '' );
+    $html_after = get_option( 'mayur_html_after', '' );
 
     ?>
     <div class="wrap">
         <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
         <form action="options.php" method="post">
             <?php
-            // Output security fields for the registered setting
             settings_fields( 'mayur_options' );
-            // Output setting sections and their fields
             do_settings_sections( 'mayur_options' );
+            wp_nonce_field( 'mayur_update_html', 'mayur_nonce' );
             ?>
             <table class="form-table">
                 <tr>
@@ -62,13 +63,37 @@ function mayur_html_notification_settings_page() {
 }
 
 add_action( 'admin_init', 'mayur_register_settings' );
-
 function mayur_register_settings() {
-    // Register a new setting for "mayur" page
-    register_setting( 'mayur_options', 'mayur_html_before' );
-    register_setting( 'mayur_options', 'mayur_html_after' );
+    register_setting( 'mayur_options', 'mayur_html_before', 'sanitize_textarea_field' );
+    register_setting( 'mayur_options', 'mayur_html_after', 'sanitize_textarea_field' );
 }
 
-// Implement the customization in email notifications here, using the saved settings.
-// ...
+add_filter( 'gform_notification', 'mayur_customize_gform_email_notification', 10, 3 );
+function mayur_customize_gform_email_notification( $notification, $form, $entry ) {
+    if ( !current_user_can( 'manage_options' ) ) {
+        return $notification;
+    }
 
+    $html_before = get_option( 'mayur_html_before', '' );
+    $html_after = get_option( 'mayur_html_after', '' );
+
+    $notification['message'] = $html_before . $notification['message'] . $html_after;
+
+    return $notification;
+}
+
+add_action( 'admin_menu', 'mayur_html_notification_menu' );
+
+function mayur_html_notification_menu() {
+    if (class_exists('GFForms')) {
+        // Add a submenu under the "Forms" menu in Gravity Forms
+        add_submenu_page(
+            'gf_edit_forms', // The slug of the parent menu (Forms menu)
+            'HTML Notifications', // The page title
+            'HTML Notifications', // The menu title
+            'manage_options', // Capability required
+            'mayur_html_notifications', // Menu slug
+            'mayur_html_notification_settings_page' // Callback function
+        );
+    }
+}
